df501fa8fcbfca4b0f6317309f7bcffb34b5e6d3
Chloe Biddle <chloebiddle@Chloes-MacBook-Pro.local>
2024-02-24 18:00:31 -0600

    all connected - no styling

---
+++ globalFunctions.js
@@ -0,0 +1,8 @@
+var particle = new Particle();
+var myDevice = "0000"; // Photon device ID
+var myToken = "0000"; // Photon access token
+
+function showGameScreen() {
+  $(".screen").hide();
+  $("#gameScreen").show();
+}
