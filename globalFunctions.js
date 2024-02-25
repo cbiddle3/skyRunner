52cc098fbd6d84b10195ba4083b9780cb8b0624a
Chloe Biddle <chloebiddle@Chloes-MacBook-Pro.local>
2024-02-24 18:05:26 -0600

    deleted extra code: q

--- globalFunctions.js
+++ globalFunctions.js
@@ -1,7 +1,3 @@
-var particle = new Particle();
-var myDevice = "0000"; // Photon device ID
-var myToken = "0000"; // Photon access token
-
 function showGameScreen() {
   $(".screen").hide();
   $("#gameScreen").show();
