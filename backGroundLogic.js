20261413a0b100985147d6e45097480be34c3257
Amal Rizvi <amal.rizvi@slu.edu>
2024-03-18 17:35:21 -0500

    character on screen with background, needs resizing, moves on its own

--- backGroundLogic.js
+++ backGroundLogic.js
@@ -36,6 +36,42 @@ const layer1 = new Layer(backgroundLayer, .6);
 
 const gameObjects = [layer1];
 
+//new code
+
+class Character{
+    constructor(image){
+        this.x = CANVAS_WIDTH/2;
+        this.y = CANVAS_HEIGHT/2;
+        this.width = 100;
+        this.height = 100;
+        //this.width = image.width;
+        //this.height = image.height;
+        this.image = image;
+    }
+
+    update() {
+        this.x += gameSpeed-5; // characters x position based on game speed, need to change to beginning of frame
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
+
+const characterImage = new Image();
+const character = new Character(characterImage);
+
+characterImage.onload = function() {
+    character.image = characterImage;
+    gameObjects.push(character);
+    console.log("Character image loaded successfully.");
+}
+
+characterImage.onerror = function() {
+    console.error("Failed to load character image.");
+}
+
+characterImage.src = 'images/skyRunnerCharacter.png';
+//
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -43,6 +79,11 @@ function animate(){
         object.update();
         object.draw();
     });
+
+    character.update(); 
+    character.draw();
     requestAnimationFrame(animate);
 };
+
 animate();
+
