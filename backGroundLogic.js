379fba450457e6a867a67f9dc849a77602e3202f
cbiddle3 <chloebiddle@Chloes-MacBook-Pro.local>
2024-04-08 11:25:08 -0500

    fixed to standards

--- backGroundLogic.js
+++ backGroundLogic.js
@@ -1,23 +1,24 @@
 export class Background {
-    constructor(game, speedModifier){
-        this.game = game;
-        this.x = 0;
-        this.y = 0;
-        this.width = 1300;
-        this.height = 500;
-        this.image = document.getElementById('backgroundImage');
-        this.speedModifier = speedModifier;
-    }
+  constructor (game, speedModifier) {
+    this.game = game
+    this.x = 0
+    this.y = 0
+    this.width = 1300
+    this.height = 500
+    this.image = document.getElementById('backgroundImage')
+    this.speedModifier = speedModifier
+  }
 
-    update(){
-        if (this.x <= -this.width){
-            this.x = 0;
-        } else {
-            this.x -= this.game.speed * this.speedModifier;
-        }
+  update () {
+    if (this.x <= -this.width) {
+      this.x = 0
+    } else {
+      this.x -= this.game.speed * this.speedModifier
     }
-    draw(ctx){
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-    }
+  }
+
+  draw (ctx) {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
+  }
 }
--- buildings.js
+++ buildings.js
@@ -1,85 +1,84 @@
 class Building {
-    constructor() {
-        this.markedForDeletion = false;
-        this.alreadyVisited = false;
-    }
+  constructor () {
+    this.markedForDeletion = false
+    this.alreadyVisited = false
+  }
 
-    update() {
-        this.x -= this.game.speed
+  update () {
+    this.x -= this.game.speed
 
-        if (this.x + this.width < 0) {
-            this.markedForDeletion = true;
-         }
+    if (this.x + this.width < 0) {
+      this.markedForDeletion = true
     }
+  }
 
-    draw(context) {
-        if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
-        context.drawImage(this.image, this.x, this.y, this.width, this.height); 
-    }
+  draw (context) {
+    if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height)
+    context.drawImage(this.image, this.x, this.y, this.width, this.height)
+  }
 }
 
 export class BrownBuilding extends Building {
-    constructor(game) {
-        super();
-        this.game = game;
-        this.width = 100;
-        this.height = 130;
-        this.gap = 30;
-        this.image = document.getElementById('brownBuilding');
-        this.x = this.game.width;
-        this.y = this.game.height - this.height - this.game.groundMargin;
-    }
+  constructor (game) {
+    super()
+    this.game = game
+    this.width = 100
+    this.height = 130
+    this.gap = 30
+    this.image = document.getElementById('brownBuilding')
+    this.x = this.game.width
+    this.y = this.game.height - this.height - this.game.groundMargin
+  }
 }
 
 export class Cafe extends Building {
-    constructor(game) {
-        super();
-        this.game = game;
-        this.width = 150;
-        this.height = 85;
-        this.gap = 30;
-        this.image = document.getElementById('cafe');
-        this.x = this.game.width;
-        this.y = this.game.height - this.height - this.game.groundMargin;
-    }
+  constructor (game) {
+    super()
+    this.game = game
+    this.width = 150
+    this.height = 85
+    this.gap = 30
+    this.image = document.getElementById('cafe')
+    this.x = this.game.width
+    this.y = this.game.height - this.height - this.game.groundMargin
+  }
 }
 
 export class GreenBuilding extends Building {
-    constructor(game) {
-        super();
-        this.game = game;
-        this.width = 100;
-        this.height = 170;
-        this.gap = 30;
-        this.image = document.getElementById('greenBuilding');
-        this.x = this.game.width;
-        this.y = this.game.height - this.height - this.game.groundMargin;
-    }
+  constructor (game) {
+    super()
+    this.game = game
+    this.width = 100
+    this.height = 170
+    this.gap = 30
+    this.image = document.getElementById('greenBuilding')
+    this.x = this.game.width
+    this.y = this.game.height - this.height - this.game.groundMargin
+  }
 }
 
 export class SodaShop extends Building {
-    constructor(game) {
-        super();
-        this.game = game;
-        this.width = 200;
-        this.height = 90;
-        this.gap = 30;
-        this.image = document.getElementById('sodaShop');
-        this.x = this.game.width;
-        this.y = this.game.height - this.height - this.game.groundMargin;
-    }
+  constructor (game) {
+    super()
+    this.game = game
+    this.width = 200
+    this.height = 90
+    this.gap = 30
+    this.image = document.getElementById('sodaShop')
+    this.x = this.game.width
+    this.y = this.game.height - this.height - this.game.groundMargin
+  }
 }
 
 export class SushiBuilding extends Building {
-    constructor(game) {
-        super();
-        this.game = game;
-        this.width = 100;
-        this.height = 150;
-        this.gap = 30;
-        this.image = document.getElementById('sushiBuilding');
-        this.x = this.game.width;
-        this.y = this.game.height - this.height - this.game.groundMargin;
-    }
+  constructor (game) {
+    super()
+    this.game = game
+    this.width = 100
+    this.height = 150
+    this.gap = 30
+    this.image = document.getElementById('sushiBuilding')
+    this.x = this.game.width
+    this.y = this.game.height - this.height - this.game.groundMargin
+  }
 }
-
