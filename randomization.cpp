96d8624492f549f86515df577265b92b0203db7a
sarah-fidahussain-SLU <sarah.fidahussain@slu.edu>
2024-04-21 23:44:07 -0500

    attemp #3 at fixing lint errors

--- randomization.cpp
+++ randomization.cpp
@@ -1,11 +1,9 @@
-// Copyright 2024 Sarah Fidahussain
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
-#include <string>
 #include <node.h>
+#include <string>
+#include <vector>
 
 namespace randomization {
 
@@ -13,23 +11,24 @@ using v8::Array;
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
-using v8::Object;
 using v8::Number;
+using v8::Object;
 using v8::String;
 using v8::Value;
 
 struct Building {
-    std::string name; 
+    std::string name;
     int width;
     int height;
 
     Building(std::string n, int w, int h) : name(n), width(w), height(h) {}
-}; 
+};
 
 void GetRandomBuilding(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
 
     std::vector<Building> buildingTypes;
+
     buildingTypes.push_back(Building("brown-building", 100, 130));
     buildingTypes.push_back(Building("cafe", 150, 85));
     buildingTypes.push_back(Building("green-building", 100, 170));
@@ -45,18 +44,18 @@ void GetRandomBuilding(const FunctionCallbackInfo<Value>& args) {
     Local<Array> result = Array::New(isolate, 3);
 
     result->Set(isolate->GetCurrentContext(), 0, String::NewFromUtf8(isolate, building.name.c_str()).ToLocalChecked());
-    result->Set(isolate->GetCurrentContext(), 1, String::NewFromUtf8(isolate, std::to_string(building.width).c_str()).ToLocalChecked());
-    result->Set(isolate->GetCurrentContext(), 2, String::NewFromUtf8(isolate, std::to_string(building.height).c_str()).ToLocalChecked());
+    result->Set(isolate->GetCurrentContext(), 1, Number::New(isolate, building.width));
+    result->Set(isolate->GetCurrentContext(), 2, Number::New(isolate, building.height));
 
     args.GetReturnValue().Set(result);
 }
 
 void GetRandomGap(const FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = args.GetIsolate(); 
+    Isolate* isolate = args.GetIsolate();
     std::srand(std::time(nullptr));
-    
+
     int gap = std::rand() % 31 + 20;
-    auto rtngap = Number::New(isolate, gap); 
+    auto rtngap = Number::New(isolate, gap);
     args.GetReturnValue().Set(rtngap);
 }
 
@@ -66,4 +65,5 @@ void Init(Local<Object> exports) {
 }
 
 NODE_MODULE(randomization, Init)
-}
+
+}  // namespace randomization
